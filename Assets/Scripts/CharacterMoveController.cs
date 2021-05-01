6d2157856525c736a48316fb77dae95816bf8fec
yosafatrend <yosafatrendip@gmail.com>
2021-05-01 23:01:05 +0700

    first commit

---
+++ Assets/Scripts/CharacterMoveController.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoveController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveAccel;
+    public float maxSpeed;
+
+    [Header("Jump")]
+    public float jumpAccel;
+
+    [Header("Ground Raycast")]
+    public float groundRaycastDistance;
+    public LayerMask groundLayerMask;
+
+    [Header("Scoring")]
+    public ScoreController score;
+    public float scoringRatio;
+    private float lastPositionX;
+
+    [Header("GameOver")]
+    public GameObject gameOverScreen;
+    public float fallPositionY;
+
+    [Header("Camera")]
+    public CameraMoveController gameCamera;
+
+    private bool isJumping;
+    private bool isOnGround;
+
+    private Animator anim;
+
+    private Rigidbody2D rb;
+
+    private CharacterSoundController sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sound = GetComponent<CharacterSoundController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isOnGround)
+            {
+                isJumping = true;
+
+                sound.PlayJump();
+            }
+        }
+        anim.SetBool("isOnGround", isOnGround);
+
+        //calculate score
+        int distancePassed = Mathf.FloorToInt(transform.position.x - lastPositionX);
+        int scoreIncrement = Mathf.FloorToInt(distancePassed / scoringRatio);
+
+        if (scoreIncrement > 0)
+        {
+            score.IncreaseCurrentScore(scoreIncrement);
+            lastPositionX += distancePassed;
+        }
+
+        //game over
+        if (transform.position.y < fallPositionY)
+        {
+            GameOver();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //raycast ground
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down,
+            groundRaycastDistance, groundLayerMask);
+
+        if (hit)
+        {
+            if (!isOnGround && rb.velocity.y <= 0)
+            {
+                isOnGround = true;
+            }
+        }
+        else
+        {
+            isOnGround = false;
+        }
+
+        //calculate velocity vector
+        Vector2 velocityVector = rb.velocity;
+
+        if (isJumping)
+        {
+            velocityVector.y += jumpAccel;
+            isJumping = false;
+        }
+
+        velocityVector.x = Mathf.Clamp(velocityVector.x + moveAccel * Time.deltaTime, 0.0f, maxSpeed);
+
+        rb.velocity = velocityVector;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawLine(transform.position, transform.position + (Vector3.down *
+            groundRaycastDistance), Color.white);
+    }
+
+    private void GameOver()
+    {
+        //set highscore
+        score.FinishScoring();
+
+        //stop camera movement
+        gameCamera.enabled = false;
+
+        //show gameover
+        gameOverScreen.SetActive(true);
+
+        //disable this too
+        this.enabled = false;
+    }
+}
---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+    private int currentScore = 0;
+
+    [Header("Score Highlight")]
+    public int scoreHighlightRange;
+    public CharacterSoundController sound;
+
+    private int lastScoreHighlight = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentScore = 0;
+        lastScoreHighlight = 0;
+    }
+
+    public float GetCurrentScore()
+    {
+        return currentScore;
+    }
+
+    public void IncreaseCurrentScore(int increment)
+    {
+        currentScore += increment;
+
+        if (currentScore - lastScoreHighlight > scoreHighlightRange)
+        {
+            sound.PlayScoreHighlight();
+            lastScoreHighlight += scoreHighlightRange;
+        }
+    }
+
+    public void FinishScoring()
+    {
+        //set high score
+        if (currentScore > ScoreData.highScore)
+        {
+            ScoreData.highScore = currentScore;
+        }
+    }
+}
